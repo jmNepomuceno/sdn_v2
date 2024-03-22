6182ea319a55713dfbce9f2fae7a065d39bd2ec1
John Marvin Nepomuceno <jmgnngmj@gmail.com>
2024-03-22 17:00:11 +0800

    032224

--- Dummy/constants.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Define a constant for the CSRF token key (change this to a unique name)
-    define('CSRF_TOKEN_KEY', '_csrf_token');
-?>
--- php_2/session_timer.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    session_start();
-    include('../database/connection2.php');
-    $_SESSION['running_timer'] = $_POST['formattedTime'];
-    $_SESSION['running_hpercode'] = $_POST['hpercode'];
-?>
